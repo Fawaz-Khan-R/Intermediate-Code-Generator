3478592f08ba10397496d27f9495a4a3dc83d002
Fawaz Khan R <115716567+Fawaz-Khan-R@users.noreply.github.com>
2024-05-12 17:37:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 import StackClass
+import random
 
 def T_A_C(exp):
     stack = []
     x = 1
-    # an instance of the StackClass module
     obj = StackClass.Conversion(len(exp))
-    # Convert the given infix notation to postfix
     postfix = obj.infixToPostfix(exp)
     intermediate_code = ["Postfix notation: " + postfix + "\nExpressions:"]  # Store postfix notation
     
-    # Generate intermediate code based on postfix
-    for i in postfix:
-        if i in "abcdefghijklmnopqrstuvwxyz" or i in "0123456789":
-            stack.append(i)
-        elif i == '-':
-            op1 = stack.pop()
-            intermediate_code.append(f"t({x}) = {i} {op1}")
-            stack.append(f"t({x})")
-            x += 1
-            if stack:
-                op2 = stack.pop()
+    i = 0
+    while i < len(postfix):
+        if postfix[i] in "abcdefghijklmnopqrstuvwxyz" or postfix[i] in "0123456789":
+            stack.append(postfix[i])
+        elif postfix[i] == '(' and postfix[i + 1] == '-':
+            stack.append(postfix[i] + postfix[i + 1])
+            i += 1  
+        elif postfix[i] == '-':
+            if len(stack) >= 2 and stack[-2] in "+-*/":
                 op1 = stack.pop()
-                intermediate_code.append(f"t({x}) = {op1} + {op2}")
+                intermediate_code.append(f"t({x}) = {postfix[i]} {op1}")
                 stack.append(f"t({x})")
                 x += 1
-        elif i == '=':
+            else:
+                op1 = stack.pop()
+                op2 = stack.pop()
+                intermediate_code.append(f"t({x}) = {op2} {postfix[i]} {op1}")
+                stack.append(f"t({x})")
+                x += 1
+        elif postfix[i] == '=':
             op2 = stack.pop()
             op1 = stack.pop()
-            intermediate_code.append(f"{op1} {i} {op2}")
-        else:
-            op1 = stack.pop()
-            if stack:
-                op2 = stack.pop()
-                intermediate_code.append(f"t({x}) = {op2} {i} {op1}")
-                stack.append(f"t({x})")
-                x += 1
+            intermediate_code.append(f"{op1} {postfix[i]} {op2}")
+            stack.append(op1)
+        i += 1
     
     return '\n'.join(intermediate_code)
 
@@ -120,41 +118,43 @@ def generate_quadruples(postfix):
 def generate_indirect_triples(postfix):
     stack = []
     x = 0
-    results = ["{0:^10} | {1:^10} | {2:^10} | {3:^10}".format('op', 'arg1', 'arg2', 'result')]
-    for i in postfix:
-        if i.isalnum():
-            stack.append(i)
-        elif i == '-':
-            if stack:
-                op1 = stack.pop()
-                stack.append(f"({x})")
-                results.append("{0:^10} | {1:^10} | {2:^10} | {3:^10}".format(i, op1, '(-)', f"({x})"))
-                x += 1
-                if stack:
-                    op2 = stack.pop()
-                    op1 = stack.pop()
-                    results.append("{0:^10} | {1:^10} | {2:^10} | {3:^10}".format('+', op1, op2, f"({x})"))
-                    stack.append(f"({x})")
-                    x += 1
-            else:
-                results.append("Error: Insufficient operands for operator: " + i)
-                return "\n".join(results)
-        elif i == '=':
-            if len(stack) >= 2:
-                op2 = stack.pop()
-                op1 = stack.pop()
-                results.append("{0:^10} | {1:^10} | {2:^10} | {3:^10}".format(i, op2, '(-)', op1))
-            else:
-                results.append("Error: Insufficient operands for operator: " + i)
-                return "\n".join(results)
-        else:
-            if len(stack) >= 2:
-                op1 = stack.pop()
-                op2 = stack.pop()
-                results.append("{0:^10} | {1:^10} | {2:^10} | {3:^10}".format(i, op2, op1, f"({x})"))
-                stack.append(f"({x})")
-                x += 1
-            else:
-                results.append("Error: Insufficient operands for operator: " + i)
-                return "\n".join(results)
-    return "\n".join(results)
+    start_address = random.randint(1, 50)
+    current_address = start_address
+    triples_results = []
+    pointers_results = []
+
+    triples_results.append("{0:^10} | {1:^10} | {2:^10}".format('Op', 'Arg1', 'Arg2'))
+    pointers_results.append("{0:^10} | {1:^10}".format('#', 'Statement'))
+
+    i = 0
+    while i < len(postfix):
+        char = postfix[i]
+        if char.isdigit() or (char.isalpha() and (i == 0 or postfix[i-1] not in ['-', '+', '*', '/', '('])):
+            stack.append(char)
+        elif char in ['+', '-', '*', '/']:
+            if char == '-' and i + 1 < len(postfix) and postfix[i + 1] == '(':
+                i += 2  
+                if i < len(postfix) and postfix[i].isdigit():
+                    stack.append(f"-{postfix[i]}")
+                    i += 1  
+                    if postfix[i] == ')':
+                        i += 1  
+                continue
+            if len(stack) < 2:
+                return "Error: Insufficient operands for operator: " + char
+            op2 = stack.pop()
+            op1 = stack.pop()
+            triples_results.append("{0:^10} | {1:^10} | {2:^10}".format(char, op1, op2))
+            pointers_results.append("{0:^10} | {1:^10}".format(current_address, x))
+            current_address += 1
+            stack.append(f"t({x})")
+            x += 1
+        elif char == '(':
+            i += 1
+            continue
+        elif char == ')':
+            i += 1
+            continue
+        i += 1
+
+    return "\n".join(triples_results) + "\n\nPointers Table:\n" + "\n".join(pointers_results)
